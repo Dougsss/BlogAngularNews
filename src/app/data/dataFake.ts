c57a5f5531485800536917d62977bbd5e34f9c3f
Douglas Araujo <testecliente207@gmail.com>
2023-10-01 23:03:39 -0300

    add more one big-card

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,30 +7,36 @@ export const dataFake = [
   },
   {
     "id":"2",
+    "title": "Mais temporadas de One Piece chegando à Netflix em outubro de 2023",
+    "description": "Mais temporadas de One Piece serão adicionadas à Netflix em 1º de outubro de 2023. Ainda não sabemos quais arcos serão adicionados, mas com certeza teremos os arcos Saboady Archipelago e um arco filler de duas partes.",
+    "photoCover":"https://guianetflix.net/wp-content/uploads/2023/09/1695447134_Ainda-mais-temporadas-de-anime-One-Piece-chegando-a-Netflix-1140x720.jpeg.webp"
+  },
+  {
+    "id":"3",
     "title": "Monkey D. Luffy",
     "description": "Monkey D. Luffy é o protagonista do popular anime e mangá One Piece, criado por Eiichiro Oda. Ele é um jovem pirata com a habilidade de esticar seu corpo como borracha devido à ingestão da Gomu Gomu no Mi, uma das Frutas do Diabo. Determinado a se tornar o Rei dos Piratas, Luffy lidera a tripulação dos Chapéu de Palha em busca do One Piece, o tesouro supremo deixado pelo lendário pirata Gol D. Roger. Luffy é conhecido por sua personalidade extrovertida, coragem inabalável e desejo inabalável de proteger seus amigos, tornando-se um dos personagens mais icônicos do mundo dos animes e mangás.",
     "photoCover":"./assets/personagem-monkey-d-luffy.png"
   },
   {
-    "id":"3",
+    "id":"4",
     "title": "Nami",
     "description": "Nami é uma personagem chave no anime e mangá One Piece. Inicialmente introduzida como uma ladra e navegadora, ela se junta à tripulação dos Chapéu de Palha. Seu objetivo inicial era reunir dinheiro para libertar sua aldeia natal do controle de um pirata. Nami é altamente habilidosa em navegação e cartografia, e possui um profundo conhecimento sobre o clima. Ao longo da série, ela desenvolve uma forte lealdade à tripulação e é conhecida por sua inteligência, determinação e habilidades táticas, além de ser uma ótima lutadora.",
     "photoCover":"./assets/personagem-nami.png"
   },
   {
-    "id":"4",
+    "id":"5",
     "title": "Roronoa Zoro",
     "description": "Roronoa Zoro é um personagem icônico do anime e mangá One Piece. Ele é o espadachim principal da tripulação dos Chapéu de Palha, liderada por Monkey D. Luffy. Zoro é mestre em técnicas de espada de três espadas (Santoryu) e tem um sonho de se tornar o melhor espadachim do mundo. Ele é conhecido por sua grande força física, determinação inabalável e lealdade à tripulação. Além de sua habilidade excepcional com a espada, Zoro também possui uma impressionante resistência e é capaz de suportar ferimentos graves em batalha. Sua personalidade calma e focada o torna um dos membros mais confiáveis e respeitados da tripulação.",
     "photoCover":"./assets/personagem-roronoa-zoro.png"
   },
   {
-    "id":"5",
+    "id":"6",
     "title": "Sanji",
     "description": "Sanji é um personagem cativante do anime e mangá One Piece. Ele é o cozinheiro da tripulação dos Chapéu de Palha, liderada por Monkey D. Luffy. Sanji é conhecido por sua habilidade culinária excepcional e suas técnicas de luta usando suas pernas, que ele aprimorou através do estilo de luta Black Leg. Ele é extremamente dedicado às suas habilidades culinárias e à proteção da tripulação, especialmente das mulheres. Sanji é famoso por sua cortesia em relação às mulheres, se recusando a lutar contra elas. Seu charme, habilidades de combate únicas e devoção à tripulação o tornam um membro valioso e querido do grupo.",
     "photoCover":"./assets/personagem-sanji.png"
   },
   {
-    "id":"6",
+    "id":"7",
     "title": "Tony Chopper",
     "description": "Tony Tony Chopper é uma rena que comeu a Hito Hito no Mi, uma Fruta do Diabo que concede habilidades humanas, no anime e mangá One Piece. Ele é o médico da tripulação dos Chapéu de Palha e possui a capacidade de se transformar em várias formas, incluindo uma forma humana. Além de suas habilidades médicas, Chopper é um médico muito habilidoso e inventivo, e também é capaz de usar o Rumble Ball para transformações ainda mais poderosas. Ele é conhecido por sua personalidade inocente e gentil, sendo extremamente leal à tripulação e particularmente próximo de seu mentor, o Dr. Hiluluk. Chopper é amado por sua natureza adorável e sua determinação em ajudar os outros.",
     "photoCover":"./assets/personagem-tony-chopper.png"
